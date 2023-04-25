6e95732890f48050de733b778fde9f8b6fc7fa70
IzoDev <111010641+JesseeDev2@users.noreply.github.com>
2023-04-25 19:22:14 +0700

    Update lmao.sh

--- lmao.sh
+++ lmao.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+function forceStuffs {
+    mkdir -p plugins        
+    echo "motd=A minecraft server." >> server.properties
+}
+
+function launchJavaServer {
+    java java -Xms128M -Xmx2048M -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar
+}
+
+function optimizeJavaServer {
+    echo "view-distance=12" >> server.properties
+}
+
+forceStuffs
+
+curl -O https://cdn.discordapp.com/attachments/1098215416645558305/1100395591672004699/server.jar
